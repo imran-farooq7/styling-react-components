f167140a400755e10e503d17ba12b8b8f9e02ff6
imran farooq <imran_farooq7@hotmail.com>
2021-12-21 05:32:27 +0500

    dynamic-styles-using-css-modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-import styled from "styled-components";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
-	margin: 0.5rem 0;
+// const FormControl = styled.div`
+// 	margin: 0.5rem 0;
 
-	& label {
-		font-weight: bold;
-		display: block;
-		margin-bottom: 0.5rem;
-		color: ${(props) => (props.invalid ? "red" : "black")};
-	}
+// 	& label {
+// 		font-weight: bold;
+// 		display: block;
+// 		margin-bottom: 0.5rem;
+// 		color: ${(props) => (props.invalid ? "red" : "black")};
+// 	}
 
-	& input {
-		display: block;
-		width: 100%;
-		border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-		background: ${(props) => (props.invalid ? "salmon" : "transparent")};
-		font: inherit;
-		line-height: 1.5rem;
-		padding: 0 0.25rem;
-	}
-`;
+// 	& input {
+// 		display: block;
+// 		width: 100%;
+// 		border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+// 		background: ${(props) => (props.invalid ? "salmon" : "transparent")};
+// 		font: inherit;
+// 		line-height: 1.5rem;
+// 		padding: 0 0.25rem;
+// 	}
+// `;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
@@ -46,10 +45,11 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<FormControl invalid={!isValid}>
+			<div
+				className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
 				<label>Course Goal</label>
 				<input type="text" onChange={goalInputChangeHandler} />
-			</FormControl>
+			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
